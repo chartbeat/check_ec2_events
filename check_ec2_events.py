df9b26e42dde37dd592c75f19428bd50c5a1c1d2
MATSUU Takuto <matsuu@heartbeats.jp>
2013-07-26 18:01:45 +0900

    support REGION

--- check_ec2_events.py
+++ check_ec2_events.py
@@ -14,12 +14,13 @@ import re
 
 from datetime import datetime
 from datetime import timedelta
-from boto.ec2.connection import EC2Connection
+from boto.ec2 import connect_to_region
 from boto.exception import EC2ResponseError
 
 # Setup IAM User with read-only EC2 access
 KEY_ID = ""
 ACCESS_KEY = ""
+REGION = "us-east-1"
 
 OK = 0
 WARNING = 1
@@ -35,7 +36,7 @@ def get_instance(instance_id):
     @return: Instance object, or None if not found
     """
 
-    conn = EC2Connection(KEY_ID, ACCESS_KEY)
+    conn = connect_to_region(REGION, aws_access_key_id=KEY_ID, aws_secret_access_key=ACCESS_KEY)
     try:
         reservations = conn.get_all_instances([instance_id])
     except EC2ResponseError, ex:
@@ -67,7 +68,7 @@ class AmazonEventCheck(object):
                  Event, Scheduled Date) for hosts with pending events
         """
 
-        conn = EC2Connection(KEY_ID, ACCESS_KEY)
+        conn = connect_to_region(REGION, aws_access_key_id=KEY_ID, aws_secret_access_key=ACCESS_KEY)
         stats = conn.get_all_instance_status()
         ret = []
         for stat in stats:
@@ -112,16 +113,16 @@ class AmazonEventCheck(object):
         return WARNING
 
 def usage():
-    print >> sys.stderr, 'Usage: %s [-h|--help] [-A <aws_access_key_id>] [-S <aws_secret_access_key>] [-c <day>]' % sys.argv[0]
+    print >> sys.stderr, 'Usage: %s [-h|--help] [-A <aws_access_key_id>] [-S <aws_secret_access_key>] [-R <region>] [-c <day>]' % sys.argv[0]
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hA:S:c:", ["help"])
+        opts, args = getopt.getopt(sys.argv[1:], "hA:S:R:c:", ["help"])
     except getopt.GetoptError:
         usage()
         return UNKNOWN
 
-    global KEY_ID, ACCESS_KEY
+    global KEY_ID, ACCESS_KEY, REGION
 
     critical_threshold = 2
     for o, a in opts:
@@ -132,6 +133,8 @@ def main():
             KEY_ID = a
         if o in ("-S"):
             ACCESS_KEY = a
+        if o in ("-R"):
+            REGION = a
         if o in ("-c"):
             critical_threshold = int(a)
 
